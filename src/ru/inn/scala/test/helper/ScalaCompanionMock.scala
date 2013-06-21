01b0f1fc634748ea6495b5fc542797e8ef93aa2a
Bulat Fattahov <bulat.fattahov@inn.ru>
2013-06-21 18:27:55 +0400

    * Imports added.

--- src/ru/inn/scala/test/helper/ScalaCompanionMock.scala
+++ src/ru/inn/scala/test/helper/ScalaCompanionMock.scala
@@ -1,31 +1,27 @@
 package ru.inn.rbac.ru.inn.scala.test.helper
 
+import org.powermock.api.easymock.PowerMock
+import org.powermock.reflect.Whitebox
 
 /** Created by bfattahov 2013 */
-object ScalaCompanionMock
-{
-	/** Create EasyMock's mock for Scala Companion object */
-	def mock[T](clazz: Class[T]): T =
-	{
-		if (!clazz.getName.endsWith("$") || clazz.getField("MODULE$") == null)
-		{
-			throw new IllegalArgumentException("%s is not a scala companion object!".format(clazz.getName))
-		}
-		try
-		{
-			val mock: T = PowerMock.createNiceMock(clazz)
-			Whitebox.setInternalState(clazz, "MODULE$", mock)
-			mock
-		}
-		catch
-			{
-				case ex: Exception => throw new IllegalArgumentException("Some exception occured!\n Have you add " +
-					"@PrepareForTest(fullyQualifiedNames=Array(\"%s\")) in the top of your test class?".format(clazz.getName), ex)
-			}
-	}
+object ScalaCompanionMock {
+  /** Create EasyMock's mock for Scala Companion object */
+  def mock[T](clazz: Class[T]): T = {
+    if (!clazz.getName.endsWith("$") || clazz.getField("MODULE$") == null) {
+      throw new IllegalArgumentException("%s is not a scala companion object!".format(clazz.getName))
+    }
+    try {
+      val mock: T = PowerMock.createNiceMock(clazz)
+      Whitebox.setInternalState(clazz, "MODULE$", mock)
+      mock
+    }
+    catch {
+      case ex: Exception => throw new IllegalArgumentException("Some exception occured!\n Have you add " +
+        "@PrepareForTest(fullyQualifiedNames=Array(\"%s\")) in the top of your test class?".format(clazz.getName), ex)
+    }
+  }
 
-	def mock[T <: ScalaObject](obj: T): T =
-	{
-		mock(obj.getClass)
-	}
+  def mock[T <: ScalaObject](obj: T): T = {
+    mock(obj.getClass)
+  }
 }
